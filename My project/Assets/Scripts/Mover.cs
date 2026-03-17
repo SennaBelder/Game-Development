7e3e1f17548576e1dd1c5110f440542e7f643b1b
SennaBelder <9010538@student.zadkine.nl>
2026-03-17 09:27:01 +0100

    Commit game project

---
+++ My project/Assets/Scripts/Mover.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField]
+    float moveSpeed = 10f;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0.00f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
